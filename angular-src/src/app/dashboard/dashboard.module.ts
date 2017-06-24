df9ffea440e7c480c2897c1de21b20e0152aeb15
Stephen Wright <sjw948@gmail.com>
2017-06-24 18:35:51 +0100

    added dashboard & site api services, site-navbar + fixed dashboardOutlet

--- angular-src/src/app/dashboard/dashboard.module.ts
+++ angular-src/src/app/dashboard/dashboard.module.ts
@@ -2,6 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { router } from "../app.routes"
+
+// services
+import { DashboardApiService } from "./dashboard-api.service"
+
+// components
 import { DashboardViewComponent } from './components/dashboard-view/dashboard-view.component';
 import { DashboardHomeComponent } from './components/dashboard-home/dashboard-home.component';
 
@@ -10,12 +16,13 @@ import { DashboardHomeComponent } from './components/dashboard-home/dashboard-ho
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    router
   ],
   exports: [
     DashboardViewComponent
   ],
-  providers: [],
+  providers: [DashboardApiService],
   bootstrap: []
 })
 export class DashboardModule { }
--- angular-src/src/app/site/site.module.ts
+++ angular-src/src/app/site/site.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { router } from "../app.routes"
+
+// services
+import { SiteApiService } from "./site-api.service"
+
+// components
 import { SiteViewComponent } from './components/site-view/site-view.component';
 import { SiteHomeComponent } from './components/site-home/site-home.component';
 import { SiteRegisterComponent } from './components/site-register/site-register.component';
 import { SiteLoginComponent } from './components/site-login/site-login.component';
+import { SiteNavbarComponent } from './components/site-navbar/site-navbar.component';
 
 @NgModule({
-  declarations: [SiteViewComponent, SiteHomeComponent, SiteRegisterComponent, SiteLoginComponent],
+  declarations: [SiteViewComponent, SiteHomeComponent, SiteRegisterComponent, SiteLoginComponent, SiteNavbarComponent],
   exports: [
     SiteViewComponent,
-    SiteHomeComponent
+    SiteHomeComponent,
+    SiteRegisterComponent,
+    SiteLoginComponent,
+    SiteNavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +29,7 @@ import { SiteLoginComponent } from './components/site-login/site-login.component
     HttpModule,
     router
   ],
-  providers: [],
+  providers: [SiteApiService],
   bootstrap: []
 })
 export class SiteModule { }
