9c9b5c3fb38163a9d23f2b7370a4abf48fc74e85
Stephen Wright <sjw948@gmail.com>
2017-06-25 13:17:20 +0100

    notification bar navbar menu toggle now triggers menu resizing in navbar

--- angular-src/src/app/dashboard/components/dashboard-navbar/dashboard-navbar.component.ts
+++ angular-src/src/app/dashboard/components/dashboard-navbar/dashboard-navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DashboardNotificationBarComponent } from "../dashboard-notification-bar/dashboard-notification-bar.component"
+import { navbarVisible } from "../dashboard-notification-bar/dashboard-notification-bar.component"
 
 @Component({
   selector: 'app-dashboard-navbar',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardNavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+  ) {}
 
   ngOnInit() {
+
+  }
+
+  navbarStyle() {
+
+    let minHeight = (document.getElementsByClassName("top-level-navbar")[0].children.length * 45) + 50
+    if(navbarVisible) {
+      return {"max-height": "100vh", "min-height": `${minHeight}px`}
+    } else {
+      return {"max-height": "0px", "min-height": "0px"}
+    }
   }
 
 }
--- angular-src/src/app/dashboard/components/dashboard-notification-bar/dashboard-notification-bar.component.ts
+++ angular-src/src/app/dashboard/components/dashboard-notification-bar/dashboard-notification-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-notification-bar',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardNotificationBarComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  toggleNavbarVisible() {
+    navbarVisible = !navbarVisible
+    console.log(navbarVisible)
+  }
+
 }
+
+export let navbarVisible : boolean = true;
